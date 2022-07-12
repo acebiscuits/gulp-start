a40ec8b7d4d3bba7829e3f872c56107da416cc54
Николай Кадников <27roter72@gmail.com>
2022-07-12 11:04:10 +0400

    add watcher

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const { src, dest } = require('gulp');
+
+const sass = require('gulp-sass')(require('sass'))
+
+
+function style() {
+ 
+    return src('app/scss/style.scss')
+    .pipe(sass())
+    .pipe(dest('app/css'))
+
+}
+ 
+
+exports.style = style;
+
+function watch(){
+
+    gulp.watch('app/scss/*.scss', style)
+}
+    
+
+exports.watch = watch
